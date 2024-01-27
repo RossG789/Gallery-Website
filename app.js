8a42337dc02a9330d4178223f309d39bd11c7983
Ross Gray <rossgray@TechEd-0008.techeducators.co.uk>
2024-01-27 20:12:25 +0000

    fifth

--- app.js
+++ app.js
@@ -1,55 +1,75 @@
+// Variables
 const form = document.getElementById("form");
 const imgcontainer = document.getElementById("img-container");
 const thumbDis = document.getElementById("thumbs");
+const clientId = "_yyXBx_Z41nrX0ItYX2TcUQUYVDo_6Lv4gT0hwxwhTI";
+let imgArray = [];
+
+// Functions
+getRandomImg();
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   //   console.log(e);
   let query = e.target.input.value;
 
-  search(query);
+  getSearchImg(query);
 });
 
-async function search(queryParam) {
+async function getSearchImg(queryParam) {
   let response = await fetch(
-    `https://api.unsplash.com/search/photos?page=5&query=${queryParam}&client_id=_yyXBx_Z41nrX0ItYX2TcUQUYVDo_6Lv4gT0hwxwhTI`
+    `https://api.unsplash.com/search/photos?per_page=10&query=${queryParam}&client_id=${clientId}`
   );
 
   let data = await response.json();
   console.log(data);
 
-  let imgArray = data.results;
+  imgArray = data.results;
 
-  createImages(imgArray);
+  createImage(0);
   createThumbs(imgArray);
 }
 
+async function getRandomImg() {
+  let response = await fetch(
+    `https://api.unsplash.com/photos/random?count=10&client_id=${clientId}`
+  );
+
+  let data = await response.json();
+
+  imgArray = data;
+
+  createThumbs(imgArray);
+  createImage(0);
+}
+
 // Surely can be made shorter?
-function createImages(arrayImages) {
+function createImage(index) {
   imgcontainer.innerHTML = "";
-  arrayImages.forEach((img) => {
-    let imgTag = document.createElement("img");
-    imgTag.src = img.urls.regular;
-    imgcontainer.appendChild(imgTag);
-  });
+  heroImage = imgArray[index];
+  let imgTag = document.createElement("img");
+  imgTag.src = heroImage.urls.regular;
+  imgcontainer.appendChild(imgTag);
 }
 
 function createThumbs(arrayImages) {
   thumbDis.innerHTML = "";
+  let i = 0;
   arrayImages.forEach((img) => {
     let imgTag = document.createElement("img");
+
     imgTag.src = img.urls.thumb;
+    imgTag.id = i;
     thumbDis.appendChild(imgTag);
     // Event Listener + alt description// 'click', function (imgTag.src)...?
-    // imgTag.addEventListener("click", () =>
-    //   imgcontainer.appendChild(img.urls.regular)
-    // );
+    imgTag.addEventListener("click", () => createImage(imgTag.id));
+    i++;
   });
 }
 
-function displayThumb(img) {
-  createImages(img);
-}
+// function displayThumb(img) {
+//   createImages(img);
+// }
 
 // Need to add event listeners to thumbs to diplay thumb image below
 // Add outline to thumb image to shpw which image is selected
